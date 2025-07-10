c016f42da41684c2b4fbe987f0526c46c9eb422d
Docile123 <imbereyemasoo@gmail.com>
2025-07-10 17:51:32 +0200

    updating js-file

--- index.js
+++ index.js
@@ -2,16 +2,41 @@
 
 const searchbox = document.querySelector(".search input");
 const searchboxBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
 async function checkWeather(city) {
     const apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=99c4f1ff299ddee0e90d55b79e21125c&units=metric`;
      const response = await fetch(apiUrl);
-     let data = await response.json();
+     if(response.status===404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+     }else{
+        document.querySelector(".error").style.display="none";
+         let data = await response.json();
+     
 
      document.querySelector(".city").innerHTML =data.name||"Kigali";
      document.querySelector(".temp").innerHTML =`${Math.round(data.main.temp)} °C`;
      document.querySelector(".humidity").innerHTML =`${data.main.humidity}%`;
      document.querySelector(".wind").innerHTML =`${data.wind.speed} km/hr`;
+
+     if(data.weather[0].main=="Clouds"){
+          weatherIcon.src ="images/clouds.png"
+     }else if(data.weather[0].main=="Clear"){
+         weatherIcon.src ="images/clear.png"
+     }else if(data.weather[0].main=="Rain"){
+         weatherIcon.src ="images/rain.png"
+     }else if(data.weather[0].main=="Drizzle"){
+         weatherIcon.src ="images/drizzle.png"
+     }else if(data.weather[0].main=="Mist"){
+         weatherIcon.src ="images/mist.png"
+     }
+
+     document.querySelector(".weather").style.display="block"
+     }
+    
 }
+
+
 
 searchboxBtn.addEventListener("click",()=>{
 checkWeather(searchbox.value);
