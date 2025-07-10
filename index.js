8420dfeb57a31956d418f5cb9778996981462425
Docile123 <imbereyemasoo@gmail.com>
2025-07-10 17:22:58 +0200

    update js file

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 
-const apiUrl ="https://api.openweathermap.org/data/2.5/weather?q=kigali&appid=99c4f1ff299ddee0e90d55b79e21125c&units=metric";
 
-async function checkWeather() {
+const searchbox = document.querySelector(".search input");
+const searchboxBtn = document.querySelector(".search button");
+async function checkWeather(city) {
+    const apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=99c4f1ff299ddee0e90d55b79e21125c&units=metric`;
      const response = await fetch(apiUrl);
      let data = await response.json();
 
-     console.log(data);
+     document.querySelector(".city").innerHTML =data.name||"Kigali";
+     document.querySelector(".temp").innerHTML =`${Math.round(data.main.temp)} °C`;
+     document.querySelector(".humidity").innerHTML =`${data.main.humidity}%`;
+     document.querySelector(".wind").innerHTML =`${data.wind.speed} km/hr`;
 }
 
-checkWeather();
+searchboxBtn.addEventListener("click",()=>{
+checkWeather(searchbox.value);
+})
+
